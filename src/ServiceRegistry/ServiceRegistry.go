29b3810e1ee6ecb844fe0a053e7cedee2702c917
Kevin <github@magicmonkey.org>
2013-01-19 15:33:30 +0000

    Removed stupid IDE file header

--- src/ServiceRegistry/ServiceRegistry.go
+++ src/ServiceRegistry/ServiceRegistry.go
@@ -1,9 +1,2 @@
-/**
- * Created with IntelliJ IDEA.
- * User: kevin
- * Date: 19/01/13
- * Time: 14:41
- * To change this template use File | Settings | File Templates.
- */
 package ServiceRegistry
 
--- src/main.go
+++ src/main.go
@@ -1,11 +1,3 @@
-/**
- * Created with IntelliJ IDEA.
- * User: kevin
- * Date: 16/01/13
- * Time: 00:21
- * To change this template use File | Settings | File Templates.
- */
-
 package main
 
 import "fmt"
