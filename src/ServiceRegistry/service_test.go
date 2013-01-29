f0df6f2c3f6855f0d64d5892cbd63d5c61f2f0ad
Kevin <github@magicmonkey.org>
2013-01-29 01:23:32 +0000

    Made a real test, kind of

--- src/ServiceRegistry/service_test.go
+++ src/ServiceRegistry/service_test.go
@@ -3,12 +3,6 @@ package ServiceRegistry
 import "testing"
 
 func TestPass(t *testing.T) {
-}
-
-func TestFail(t *testing.T) {
-	t.Failf("Example fail")
-}
-
-func TestError(t *testing.T) {
-	t.Errorf("Example error")
+	sr := ServiceRegistry{}
+	sr.GenerateTestData()
 }
